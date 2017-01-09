8fe8c9e7efcf14f8f31c49a4ef346e8a4e1cfef4
Andy Eccleston <eandy5000@yahoo.com>
2017-01-09 15:03:01 -0600

    create test basics working

---
+++ test/reading_test.js
@@ -0,0 +1,4 @@
+const assert = require('assert')
+const User = require('../src/user')
+
+ 
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,6 +1,16 @@
 const mongoose  = require('mongoose')
 
-mongoose.connect('mongodb://localhost/users_test')
-mongoose.connection
-    .once('open', () => console.log('Connection open'))
-    .on('error', () => console.warn('Error', error))
+mongoose.Promise = global.Promise
+
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test')
+    mongoose.connection
+        .once('open', () => done())
+        .on('error', () => console.warn('Error', error))
+})
+
+beforeEach((done) => {
+    mongoose.connection.collections.users.drop(() => {
+        done()
+    })
+})
